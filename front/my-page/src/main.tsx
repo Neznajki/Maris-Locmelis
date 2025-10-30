af2f726906fccefb531e339defb8c7f676c4aa97
Maris Locmelis <neznajki@gmail.com>
2025-10-30 23:20:43 +0200

    fixed build reverted trash suggestion change from chat GPT

--- front/my-page/src/main.tsx
+++ front/my-page/src/main.tsx
@@ -1,29 +1,14 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  createHashRouter,
-  RouterProvider,
-} from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './styles/index.css'
 
-const router = createHashRouter(
-  [
-    {
-      path: '/*',
-      element: <App />,
-    },
-  ],
-  {
-    future: {
-      v7_startTransition: true,
-      v7_relativeSplatPath: true,
-    },
-  }
-)
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 )
