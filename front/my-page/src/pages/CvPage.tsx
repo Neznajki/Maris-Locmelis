981dab8043d3b7c8adc7764d03b590e0a6f3ce2a
Maris Locmelis <neznajki@gmail.com>
2025-11-14 20:16:06 +0200

    updated section WhatsDone

--- front/my-page/src/pages/CvPage.tsx
+++ front/my-page/src/pages/CvPage.tsx
@@ -11,6 +11,7 @@ export const Cv: React.FC = () => {
   return (
     <PageContainer title={CvTitle}>
       <div style={{ padding: 16 }}>
+        found bug in CV filled from memory wrong dates probably date of leave from pingnetgames.com was 2013.12.?? so joining date was 2011.03.?? will update when eds.vid.gov.lv will work.
         <div className="popup-spoiler">
           <button className="popup-trigger">
             <a style={{ color: "#ededed" }} href={CvPdf} download="Maris_Locmelis_2025_10_19_CV.pdf" className="text-gray-600">Download pdf CV updated  {GetServerFileTime(CvPdf)}</a>
--- front/my-page/src/pages/WhatsDonePage.tsx
+++ front/my-page/src/pages/WhatsDonePage.tsx
@@ -18,18 +18,50 @@ export const WhatsDonePage: React.FC = () => {
           ))}
       <br/><br/>
       <PopupSpoiler title="PingnetGames">
-        <Paragraph key="piratello_game">
+        Social Network Games (created everything everything related to server) <br/>
+        Please notice payments systems and admin pages where we are editing is also server side tasks <br/>
+        All I had here ssh connection to some magic PC and I was forced to make it work. No help besides google and 1 magic kick from top.<br/>
+        there was some small help when Lines of Life kicked off and got 40k+ stable online (my knowledge was not enough to configure high load SQL server) (solution cache writes and flush them once 10-20 seconds, was enough to handle 40k+ users without any locks)<br/>
+        FYI: Game Lines of Life registered users got from 0 to 1kk(1 000 000)+ in about 2 weeks, so probably it is normal <br/>
+
+        <PopupSpoiler title="Piratello">
+          <a href="https://m.vk.com/club45597928" target="_blank">
+            Piratello (official game group)
+          </a><br />
+
           <a href="/piratello_game.html" target="_blank">
-            Social Network Game Piratello
-          </a>{" "}
-          &lt;-- everything related to server
-        </Paragraph>
-        <Paragraph key="lines_of_life">
+             Piratello (cached version)
+          </a><br />
+          &lt;-- created everything everything related to server
+        </PopupSpoiler>
+        {"      "}
+        <PopupSpoiler title="Guess Word">
+          (everything related to development was implemented by me including admin page where we structure game levels, with FE everything works. (vanilla JS)<br/>
+          <a href="https://www.draugiem.lv/applications/15013460/" target="_blank">
+            Guess Word (draugiem.lv)
+          </a><br />
+          <a href="/guess_word.html" target="_blank">
+            Guess Word (cached version)
+          </a><br />
+
+          had prod ready implementation in <a href="https://nk.pl/">nk.pl maybe not launched</a>
+          <br />
+          had prod ready implementation in <a href="https://m.vk.com/">vk.com launched</a>
+          <br />
+          had prod ready implementation in <a href="https://fb.com/">fb.com not launched (did not pass verification or whatever)</a>
+        </PopupSpoiler>
+        {"      "}
+        <PopupSpoiler title="Lines of Life">
+          <a href="https://m.ok.ru/group/52959658967161" target="_blank">
+            lines of life (official game group)
+          </a><br />
+
           <a href="/lines_of_life.html" target="_blank">
-            Social Network Game lines of life
-          </a>{" "}
-          &lt;-- everything related to server
-        </Paragraph>
+            lines of life (cached version)
+          </a><br />
+          &lt;-- created everything everything related to server up to point (... found bug in CV filled from memory wrong dates probably date of leave was 2013.12.?? so joining date was 2011.03.??)
+        </PopupSpoiler>
+
       </PopupSpoiler>
       <br />
       <br />
