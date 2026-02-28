02750e058d4831167c0c84aa16b1aa8dbd3aec5c
Maris Locmelis <neznajki@gmail.com>
2026-02-28 09:52:01 +0200

    made header better for display.

--- front/my-page/src/components/page/type/SectionPart.tsx
+++ front/my-page/src/components/page/type/SectionPart.tsx
@@ -4,7 +4,7 @@ import {SectionPartType} from "@/contract/PageType";
 export const SectionPart: React.FC<SectionPartType> = ({nodes, displayText}) => {
     return (
         <div>
-            <h2>{displayText}</h2>
+            <h2 style={{ textAlign: 'center', fontWeight: 'bold' }}>{displayText}</h2>
             {nodes.map(n => n)}
         </div>
     )
