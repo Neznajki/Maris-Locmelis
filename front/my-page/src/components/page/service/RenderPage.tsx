9fd8130386242f1364061dc7e901b112167ae296
Maris Locmelis <neznajki@gmail.com>
2026-02-28 09:47:25 +0200

    made small code cleanup.

--- front/my-page/src/components/page/service/RenderPage.tsx
+++ front/my-page/src/components/page/service/RenderPage.tsx
@@ -2,34 +2,14 @@
 import React, {ReactNode, useEffect, useState} from 'react'
 import {PageSection} from "@/contract/PageSection";
 import {PageContainer} from "@/components/PageRenderer";
-import getRenderComponent from "@/components/page/service/DetectorPage";
-import {SectionPart} from "@/components/page/type/SectionPart";
+import parsePageToElements from "@/components/page/service/ParsingPage";
 
 export const RenderPage: React.FC<{ pageSectionContent: PageSection }> = ({pageSectionContent}) => {
     const [jsxElements, setJsxElements] = useState<ReactNode[]>([]);
 
     useEffect(() => {
-        const load = async () => {
-            const result = [];
-
-            for(var element of pageSectionContent.parts) {
-                var a = getRenderComponent(element);
-                var jsxChildElements =  [];
-
-                for(var content of element.contents) {
-                    var parsedContent = a.parseContent(content.content);
-
-                    jsxChildElements.push(a.render(parsedContent));
-                }
-
-                result.push(await SectionPart({nodes: jsxChildElements, displayText: element.displayText}));
-            }
-
-            setJsxElements(result);
-        };
-
-        load();
-    }, []);
+        setJsxElements(parsePageToElements(pageSectionContent));
+    }, [pageSectionContent]);
 
     return (
         <PageContainer title={pageSectionContent.title}>
