991c5e5bb170a171e3d642c40307e0c046517eb9
Maris Locmelis <neznajki@gmail.com>
2026-03-11 08:58:41 +0200

    reduced warning

--- front/my-page/src/pages/HomePage.tsx
+++ front/my-page/src/pages/HomePage.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import PageByPath from "@/pages/PageByPath";
 
-export const Home: React.FC<{ title?: React.ReactNode }> = ({ title }) => {
+export const Home: React.FC<{ title?: React.ReactNode }> = ({  }) => {
   return ( <PageByPath path="/" /> )
 }
 export default Home
